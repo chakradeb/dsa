58d5e465508fddd88d714b8e2bd1886642d47a49
Debarun Chakraborty <debarun.chakraborty96@gmail.com>
2023-01-09 17:17:38 +0530

    Add edge cases and error handling for array implementation

--- array/array.go
+++ array/array.go
@@ -1,5 +1,10 @@
 package array
 
+import (
+	"errors"
+	"math"
+)
+
 type Array struct {
 	Length int
 	Data   map[int]int
@@ -17,23 +22,46 @@ func (a *Array) Add(element int) {
 	a.Length++
 }
 
-func (a Array) Get(index int) int {
-	return a.Data[index]
+func (a Array) Get(index int) (int, error) {
+	if a.Length == 0 {
+		return a.emptyArrayAlert()
+	}
+	if index >= a.Length {
+		return a.invalidArrayIndexAlert()
+	}
+	return a.Data[index], nil
 }
 
-func (a *Array) RemoveLast() int {
+func (a *Array) RemoveLast() (int, error) {
+	if a.Length == 0 {
+		return a.emptyArrayAlert()
+	}
 	last := a.Data[a.Length-1]
 	delete(a.Data, a.Length-1)
 	a.Length--
-	return last
+	return last, nil
 }
 
-func (a *Array) Remove(index int) int {
+func (a *Array) Remove(index int) (int, error) {
+	if a.Length == 0 {
+		return a.emptyArrayAlert()
+	}
+	if index >= a.Length {
+		return a.invalidArrayIndexAlert()
+	}
 	element := a.Data[index]
 	for index+1 < a.Length {
 		a.Data[index] = a.Data[index+1]
 		index++
 	}
 	a.RemoveLast()
-	return element
+	return element, nil
+}
+
+func (a Array) emptyArrayAlert() (int, error) {
+	return -math.MaxInt, errors.New("array is empty")
+}
+
+func (a Array) invalidArrayIndexAlert() (int, error) {
+	return -math.MaxInt, errors.New("invalid array index")
 }
--- array/array_test.go
+++ array/array_test.go
@@ -1,6 +1,7 @@
 package array
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,9 +18,24 @@ func TestArray_Add(t *testing.T) {
 func TestArray_Get(t *testing.T) {
 	arr := NewArray()
 	arr.Add(5)
-	arr.Add(9)
-	assert.Equal(t, 5, arr.Get(0), "unexpected element")
-	assert.Equal(t, 9, arr.Get(1), "unexpected element")
+	actual, err := arr.Get(0)
+	assert.Equal(t, 5, actual, "unexpected element")
+	assert.NoError(t, err, "unexpected error")
+}
+
+func TestArray_Get_WhenEmptyArray(t *testing.T) {
+	arr := NewArray()
+	actual, err := arr.Get(0)
+	assert.Equal(t, -math.MaxInt, actual, "invalid element")
+	assert.EqualError(t, err, "array is empty", "invalid error message")
+}
+
+func TestArray_Get_WhenInvalidIndex(t *testing.T) {
+	arr := NewArray()
+	arr.Add(5)
+	actual, err := arr.Get(3)
+	assert.Equal(t, -math.MaxInt, actual, "invalid element")
+	assert.EqualError(t, err, "invalid array index", "invalid error message")
 }
 
 func TestArray_Remove(t *testing.T) {
@@ -28,10 +44,32 @@ func TestArray_Remove(t *testing.T) {
 	arr.Add(6)
 	arr.Add(9)
 	assert.Equal(t, 3, arr.Length, "unexpected length")
-	assert.Equal(t, 6, arr.Get(1), "unexpected element")
-	arr.Remove(1)
+	assert.Equal(t, 6, arr.Data[1], "unexpected element")
+	element, err := arr.Remove(1)
 	assert.Equal(t, 2, arr.Length, "unexpected length")
-	assert.Equal(t, 9, arr.Get(1), "unexpected element")
+	assert.Equal(t, 6, element, "unexpected length")
+	assert.Equal(t, 9, arr.Data[1], "unexpected element")
+	assert.NoError(t, err, "unexpected error")
+}
+
+func TestArray_Remove_WhenEmptyArray(t *testing.T) {
+	arr := NewArray()
+	assert.Equal(t, 0, arr.Length, "unexpected length")
+	element, err := arr.Remove(1)
+	assert.Equal(t, 0, arr.Length, "unexpected length")
+	assert.Equal(t, -math.MaxInt, element, "unexpected length")
+	assert.EqualError(t, err, "array is empty", "unexpected error")
+}
+
+func TestArray_Remove_WhenInvalidIndex(t *testing.T) {
+	arr := NewArray()
+	arr.Add(3)
+	assert.Equal(t, 1, arr.Length, "unexpected length")
+	assert.Equal(t, 3, arr.Data[0], "unexpected element")
+	element, err := arr.Remove(5)
+	assert.Equal(t, 1, arr.Length, "unexpected length")
+	assert.Equal(t, -math.MaxInt, element, "unexpected length")
+	assert.EqualError(t, err, "invalid array index", "unexpected error")
 }
 
 func TestArray_RemoveLast(t *testing.T) {
@@ -40,8 +78,18 @@ func TestArray_RemoveLast(t *testing.T) {
 	arr.Add(6)
 	arr.Add(9)
 	assert.Equal(t, 3, arr.Length, "unexpected length")
-	assert.Equal(t, 9, arr.Get(arr.Length-1), "unexpected element")
-	arr.RemoveLast()
+	assert.Equal(t, 9, arr.Data[arr.Length-1], "unexpected element")
+	actual, err := arr.RemoveLast()
 	assert.Equal(t, 2, arr.Length, "unexpected length")
-	assert.Equal(t, 6, arr.Get(arr.Length-1), "unexpected element")
+	assert.Equal(t, 9, actual, "unexpected length")
+	assert.Equal(t, 6, arr.Data[arr.Length-1], "unexpected element")
+	assert.NoError(t, err, "unexpected error")
+}
+
+func TestArray_RemoveLast_WhenEmptyArray(t *testing.T) {
+	arr := NewArray()
+	assert.Equal(t, 0, arr.Length, "unexpected length")
+	actual, err := arr.RemoveLast()
+	assert.Equal(t, -math.MaxInt, actual, "unexpected length")
+	assert.EqualError(t, err, "array is empty", "unexpected error")
 }
